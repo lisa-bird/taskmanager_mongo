06b6359e2a73b3bad7eede61773d0a1574fc4b79
lisa-bird <>
2023-07-15 13:50:16 +0000

    trying to fix 403 error

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/profile/<username>", methods=["GET", "POST-"])
+@app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
